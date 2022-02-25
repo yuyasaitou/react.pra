fd7c0a85f54efad84995aee6ae000c5d6efd0ff1
unknown <saitouyuya0903@gmail.com>
2022-02-25 19:52:58 +0900

    redux

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+export const INCREMENT ='INCREMENT'
+export const DECREMENT ='DECREMENT'
+
+export const incremnt=()=>({
+    type:'INCREMENT'
+    })
+export const decremnt=()=>({
+        type:'DECREMENT'
+})
